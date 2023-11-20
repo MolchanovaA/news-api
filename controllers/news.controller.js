908fef64a78094f1d47936dfd58e5c05c2197e8e
Alina <a.08pon@gmail.com>
2023-11-20 16:06:38 +0000

    task-3 done

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,7 +1,53 @@
-const { receiveAllTopics } = require("../modulles/news.modules");
+const { end } = require("../db/connection");
+const {
+  receiveAllTopics,
+  receiveAllEndpoints,
+  toAddEndpointsInfo,
+  writeEndpoints,
+} = require("../modulles/news.modules");
 
 exports.getAllTopics = (req, res) => {
   receiveAllTopics().then((topics) => {
-    return res.status(200).send({ topics: topics });
+    res.status(200).send({ topics: topics });
   });
+};
+
+exports.getAvailableEndpoints = (req, res) => {
+  receiveAllEndpoints().then((endpoints) => {
+    const parsedListOfEndpoints = JSON.parse(endpoints);
+    const toCorrectPathsIsNeed = checkPropertiesOfEndpoints(
+      parsedListOfEndpoints
+    );
+
+    if (toCorrectPathsIsNeed) {
+      const updatedEndpoints = toAddEndpointsInfo(
+        JSON.parse(endpoints),
+        toCorrectPathsIsNeed
+      );
+      writeEndpoints(updatedEndpoints).then(() => {
+        res.status(200).send({ listOfEndpoints: updatedEndpoints });
+      });
+    } else {
+      res.status(200).send({ listOfEndpoints: endpoints });
+    }
+  });
+};
+
+const checkPropertiesOfEndpoints = (endpointsObject) => {
+  const necessaryProperties = ["description", "queries", "exampleResponse"];
+  const pathsNeedToUpdate = [];
+
+  for (key in endpointsObject) {
+    const currentPath = {};
+    currentPath[key] = [];
+
+    necessaryProperties.forEach((property) => {
+      if (!endpointsObject[key][property]) {
+        currentPath[key].push(property);
+      }
+    });
+    if (currentPath[key].length) pathsNeedToUpdate.push(currentPath);
+  }
+
+  return !pathsNeedToUpdate.length ? false : pathsNeedToUpdate;
 };
--- modulles/news.modules.js
+++ modulles/news.modules.js
@@ -1,7 +1,30 @@
 const db = require("../db/connection");
+const fs = require("fs/promises");
 
 exports.receiveAllTopics = (req, res) => {
   return db.query("SELECT * FROM topics;").then(({ rows }) => {
     return rows;
   });
+};
+
+exports.receiveAllEndpoints = (req, res) => {
+  const endpointsFile = "endpoints.json";
+  return fs.readFile(endpointsFile, "utf-8").then((dataEndpoints) => {
+    return dataEndpoints;
+  });
+};
+
+exports.toAddEndpointsInfo = (listOfAllEndpoints, corrections) => {
+  corrections.forEach((endpoint) => {
+    const path = Object.keys(endpoint)[0];
+    endpoint[path].forEach((missedProperty) => {
+      listOfAllEndpoints[path][missedProperty] = "some description";
+    });
+  });
+  return listOfAllEndpoints;
+};
+
+exports.writeEndpoints = (endpoints) => {
+  const endpointsFile = "endpoints.json";
+  return fs.writeFile(endpointsFile, JSON.stringify(endpoints, 2));
 };
